1f7437072c01c417c2fa1e90bbef3e6e176f8350
Jesse Herrera <jessalexherr@gmail.com>
2024-03-01 12:50:15 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const icon = document.querySelector(".menu-links");
+    const menu = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
